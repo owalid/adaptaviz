01451c7dd1ffbf9bf8f8294afc3ae700b5da48d9
owalid <othmane.el.ayadi@gmail.com>
2021-12-04 00:36:59 +0100

    add init pyplot (#4)

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,12 +3,21 @@
     <v-row>
       <hello-world />
     </v-row>
+    <v-row>
+      <client-only>
+        <pyplot-example />
+      </client-only>
+    </v-row>
   </v-container>
 </template>
 <script>
 import HelloWorld from '~/components/HelloWorld'
+
 export default {
   name: "Home",
-  components: { HelloWorld }
+  components: {
+    PyplotExample: () => import('~/lazy-components/PyplotExample/PyplotExample'),
+    HelloWorld,
+  }
 }
 </script>
