48c7eb292d846828e6bb7b82bb3e4f25b6876b62
owalid <othmane.el.ayadi@gmail.com>
2021-12-03 20:34:51 +0100

    init frontend (#1)
    * init frontend
    
    * clean code

---
+++ frontend/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-row>
+      <hello-world />
+    </v-row>
+  </v-container>
+</template>
+<script>
+import HelloWorld from '~/components/HelloWorld'
+export default {
+  name: "Home",
+  components: { HelloWorld }
+}
+</script>
