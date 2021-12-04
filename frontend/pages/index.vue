2ffee4a319ee6410ea5ba5d5612724d85997894f
owalid <othmane.el.ayadi@gmail.com>
2021-12-04 03:30:57 +0100

    init map frontend

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,23 +1,17 @@
 <template>
-  <v-container>
-    <v-row>
-      <hello-world />
-    </v-row>
+  <div>
     <v-row>
       <client-only>
-        <pyplot-example />
+        <pyplot-map />
       </client-only>
     </v-row>
-  </v-container>
+  </div>
 </template>
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
 export default {
   name: "Home",
   components: {
-    PyplotExample: () => import('~/lazy-components/PyplotExample/PyplotExample'),
-    HelloWorld,
+    PyplotMap: () => import('~/lazy-components/map/PyplotMap'),
   }
 }
 </script>
