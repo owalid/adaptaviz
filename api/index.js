920edc25242e85ea291f32ef93ac47a25c425dfe
owalid <othmane.el.ayadi@gmail.com>
2021-12-05 10:35:43 +0100

    test

--- api/index.js
+++ api/index.js
@@ -2,10 +2,23 @@
 import express from 'express'
 import helmet from 'helmet'
 import { cloneDeep, findIndex } from 'lodash'
+import { MongoClient } from "mongodb";
 import cors from 'cors'
 import smallRegions from '../static/data/small-regions'
-import scoresWithAnomaly from '../static/data/scores-with-anomaly'
-import scoresWithoutAnomaly from '../static/data/scores-without-anomaly'
+// import scoresWithAnomaly from '../static/data/scores-with-anomaly'
+// import scoresWithoutAnomaly from '../static/data/scores-without-anomaly'
+
+const connectionString = process.env.ATLAS_URI;
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+client.connect((err, db) => {
+  if (err) return;
+  dbConnection = db.db("ScoresWithAnomaly");
+});
 
 // Create express instance
 const app = express()
@@ -16,10 +29,10 @@ app.use(express.json());
 app.use(helmet())
 
 app.post('/get-geojson', (req, res) => {
-  const {scenario, horizon, specie, scoreType, anomaly} = req.body
-  
-  const smallRegionsClone = cloneDeep(smallRegions);
-  const scoresClone = (anomaly) ? cloneDeep(scoresWithAnomaly) : cloneDeep(scoresWithoutAnomaly)
+  const {scenario, horizon, specie, scoreType} = req.body
+
+  const smallRegionsClone = cloneDeep(smallRegions)
+  const scoresClone = dbConnection.collection("Scores").find();
   scoresClone.forEach(scoreElmt => {
     if (scoreElmt['Scenario'] === scenario
         && scoreElmt['Horizon'] === horizon
